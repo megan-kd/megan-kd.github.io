dc1ea483880740ca69c2e4209833e1796371532b
Megan DeYoung <shastadey@gmail.com>
2023-11-01 15:26:45 -0700

    added constant for appbar offset value

--- megan-kd.github.io/src/navbar/Navbar.js
+++ megan-kd.github.io/src/navbar/Navbar.js
@@ -3,6 +3,7 @@ import { AppBar, Button, Toolbar, Box } from '@mui/material';
 import { Link } from "react-scroll";
 
 const Navbar = () => {
+    const appBarOffset = -70;
     return (
         <>
             <AppBar position={'sticky'} id={'appbar'}>
@@ -11,22 +12,22 @@ const Navbar = () => {
                         <Button color='inherit'>Home</Button>
                     </Box>
                     <Box sx={{ marginLeft: "auto" }}>
-                        <Link to="about_me" spy={true} smooth={true} offset={-70} duration={500}>
+                        <Link to="about_me" spy={true} smooth={true} offset={appBarOffset} duration={500}>
                             <Button color='inherit'>
                                 About Me
                             </Button>
                         </Link>
-                        <Link to="skills" spy={true} smooth={true} offset={-70} duration={500}>
+                        <Link to="skills" spy={true} smooth={true} offset={appBarOffset} duration={500}>
                             <Button color='inherit'>
                                 Skills
                             </Button>
                         </Link>
-                        <Link to="projects" spy={true} smooth={true} offset={-70} duration={500}>
+                        <Link to="projects" spy={true} smooth={true} offset={appBarOffset} duration={500}>
                             <Button color='inherit'>
                                 Projects
                             </Button>
                         </Link>
-                        <Link to="contact" spy={true} smooth={true} offset={-70} duration={500}>
+                        <Link to="contact" spy={true} smooth={true} offset={appBarOffset} duration={500}>
                             <Button color='inherit'>
                                 Contact
                             </Button>
