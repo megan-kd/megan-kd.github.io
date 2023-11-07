3641d4d99314bcadc4ee77c0b1b923255e26b219
Megan DeYoung <shastadey@gmail.com>
2023-11-07 14:48:39 -0800

    switched home button to home icon

--- megan-kd.github.io/src/navbar/Navbar.js
+++ megan-kd.github.io/src/navbar/Navbar.js
@@ -1,40 +1,50 @@
 import React from 'react'
-import { AppBar, Button, Toolbar, Box } from '@mui/material';
+import { AppBar, Button, Toolbar, Box, Stack, IconButton } from '@mui/material';
 import { Link, animateScroll as scroll } from "react-scroll";
+import HomeIcon from '@mui/icons-material/Home';
 
 const Navbar = () => {
     const appBarOffset = -70;
     const scrollToTop = () => {
         scroll.scrollToTop();
-    }
+    };
+
+    const buttonStyle = {
+        minWidth: '10px'
+    };
+
     return (
         <>
             <AppBar position={'sticky'} id={'appbar'}>
                 <Toolbar >
                     <Box sx={{ marginRight: "auto" }} >
-                        <Button color='inherit' onClick={scrollToTop}>Home</Button>
+                        <IconButton color='inherit' onClick={scrollToTop}>
+                            <HomeIcon fontSize='large'/>
+                        </IconButton>
                     </Box>
                     <Box sx={{ marginLeft: "auto" }}>
-                        <Link to="about_me" spy={true} smooth={true} offset={appBarOffset} duration={500}>
-                            <Button color='inherit'>
-                                About Me
-                            </Button>
-                        </Link>
-                        <Link to="skills" spy={true} smooth={true} offset={appBarOffset} duration={500}>
-                            <Button color='inherit'>
-                                Skills
-                            </Button>
-                        </Link>
-                        <Link to="projects" spy={true} smooth={true} offset={appBarOffset} duration={500}>
-                            <Button color='inherit'>
-                                Projects
-                            </Button>
-                        </Link>
-                        <Link to="contact" spy={true} smooth={true} offset={appBarOffset} duration={500}>
-                            <Button color='inherit'>
-                                Contact
-                            </Button>
-                        </Link>
+                        <Stack direction={'row'} spacing={{xs: 1, sm: 3.5, md: 2}}>
+                            <Link to="about_me" spy={true} smooth={true} offset={appBarOffset} duration={500}>
+                                <Button color='inherit' style={buttonStyle}>
+                                    About Me
+                                </Button>
+                            </Link>
+                            <Link to="skills" spy={true} smooth={true} offset={appBarOffset} duration={500}>
+                                <Button color='inherit'>
+                                    Skills
+                                </Button>
+                            </Link>
+                            <Link to="projects" spy={true} smooth={true} offset={appBarOffset} duration={500}>
+                                <Button color='inherit'>
+                                    Projects
+                                </Button>
+                            </Link>
+                            <Link to="contact" spy={true} smooth={true} offset={appBarOffset} duration={500}>
+                                <Button color='inherit'>
+                                    Contact
+                                </Button>
+                            </Link>
+                        </Stack>
                     </Box>
                 </Toolbar>
             </AppBar>
